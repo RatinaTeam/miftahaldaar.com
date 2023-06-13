ef98c751feec0c78e11b7b56e9c502a93cc7e6f4
daalimouhcine <the.daali.mouhcine@gmail.com>
2023-06-13 16:06:34 +0200

    don't trigger navigation to detail page if user clicks on button

--- src/components/Dashboard/AllOrders.jsx
+++ src/components/Dashboard/AllOrders.jsx
@@ -1,253 +1,281 @@
-import { Button, Card, Typography } from "@material-tailwind/react";
-import { SearchField } from "../Shared/StyledComponents";
-import { Link, useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
-import axios from "axios";
-import { AuthContext } from "../../contexts/AuthProvider";
-import { useContext, useEffect, useState } from "react";
+import { Button, Card, Typography } from '@material-tailwind/react'
+import { SearchField } from '../Shared/StyledComponents'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import axios from 'axios'
+import { AuthContext } from '../../contexts/AuthProvider'
+import { useContext, useEffect, useRef, useState } from 'react'
 
 export default function AllOrders({ allOrders, handleOpenAddUpdates }) {
-    const { userID,loggedUser } = useContext(AuthContext);
-    const [usersList, setUsersList] = useState([]);
-    const order_status_translations = {
-        "PENDING": "معلق",
-        "CANCELLED": "ملغى",
-        "PARTIALLY_COMPLETED": "منجز جزئياً",
-        "COMPLETED": "منجز",
-        "ON_PROGRESS": "قيد التنفيذ",
-        "DELAYED": "متأخر"
-    }
-    
-    const fetchData = async () => {
-        axios.get("https://miftahaldaar.ratina.co/user/all",
-          
-        { headers }).then((res) => {
-      
-        setUsersList(res.data.users);
-       }).catch((err) => {
-           console.log(err);
-       })
-    };
-    useEffect(() => {
-        fetchData();
-    }, []);
-    
-    const filterUsers = (id) => {
-        return usersList?.filter((user) => user.id === id)[0].username;
-    };
-
-    
-const headers = {
-    "user-id": userID,
-    "auth-key": "sdofmasdmfasdmflkmasdf",
-  };
-    const handleAssign =async (
-        order_id
-    ) => {
- 
-    
-
-        const { value: selectedValue } = await Swal.fire({
-            title: 'تعيين موظف',
-            input: 'select',
-            inputOptions: {
-                'المستخدمون': usersList.map((user) => {
-                    return user.username;
-                  })            
-            },
-            inputPlaceholder: 'اختر موظف',
-            showCancelButton: true,
-            confirmButtonText: "تعيين",
-            cancelButtonText: "إغلاق",
-        })
-        
-
-        if (selectedValue) {
-        const formData = new FormData();
-        formData.append("order_id", order_id);
-        formData.append("emp_id", usersList[selectedValue].id);
-        await axios.post("https://miftahaldaar.ratina.co/order_status/assign",
-            formData,
-            { headers }).then((res) => {
-                console.log(res.data)
-            }).catch((err) => {
-                console.log(err);
-            })
-    Swal.fire(`${usersList[selectedValue].username} : لقد حددت`)
+  const { userID, loggedUser } = useContext(AuthContext)
+  const [usersList, setUsersList] = useState([])
+  const order_status_translations = {
+    PENDING: 'معلق',
+    CANCELLED: 'ملغى',
+    PARTIALLY_COMPLETED: 'منجز جزئياً',
+    COMPLETED: 'منجز',
+    ON_PROGRESS: 'قيد التنفيذ',
+    DELAYED: 'متأخر',
   }
-    };
-  
 
-    const TABLE_HEAD = [
-        {
-            arabic: "",
-        },
-        {
-            arabic: "الملاحظات",
-        },
-        {
-            arabic: "الموظف",
-        },
-        {
-            arabic: "حالة الطلب",
-        },
-        {
-            arabic: "تاريخ إعادة المعالجة",
-        },
-        {
-            arabic: "بنك الراتب",
-        },
-        {
-            arabic: "رقم الحوال",
-        },
-        {
-            arabic: "العميل",
-        },
-        {
-            arabic: "نوع الطلب",
-        },
-        {
-            arabic: "بطاقة تعريف",
-        },
-    ];
+  const fetchData = async () => {
+    axios
+      .get(
+        'https://miftahaldaar.ratina.co/user/all',
 
-    const TABLE_ROWS = allOrders;
+        { headers }
+      )
+      .then((res) => {
+        setUsersList(res.data.users)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-    const navigate = useNavigate();
-    const navigateToRow = (id) => {
-        navigate(`/dashboard/order_details?order_id=${id}`);
+  const filterUsers = (id) => {
+    return usersList?.filter((user) => user.id === id)[0].username
+  }
+
+  const headers = {
+    'user-id': userID,
+    'auth-key': 'sdofmasdmfasdmflkmasdf',
+  }
+  const handleAssign = async (order_id) => {
+    const { value: selectedValue } = await Swal.fire({
+      title: 'تعيين موظف',
+      input: 'select',
+      inputOptions: {
+        المستخدمون: usersList.map((user) => {
+          return user.username
+        }),
+      },
+      inputPlaceholder: 'اختر موظف',
+      showCancelButton: true,
+      confirmButtonText: 'تعيين',
+      cancelButtonText: 'إغلاق',
+    })
+
+    if (selectedValue) {
+      const formData = new FormData()
+      formData.append('order_id', order_id)
+      formData.append('emp_id', usersList[selectedValue].id)
+      await axios
+        .post('https://miftahaldaar.ratina.co/order_status/assign', formData, {
+          headers,
+        })
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      Swal.fire(`${usersList[selectedValue].username} : لقد حددت`)
     }
+  }
 
-    return (
-        <Card className="overflow-auto h-full w-full">
-            <table className="w-full min-w-max table-auto text-left">
-                <thead>
-                    <tr>
-                        {TABLE_HEAD.map((head, i) => (
-                            <th key={i} className="border-b border-blue-gray-100 bg-blue-gray-50 p-3   ">
-                                <div className="flex flex-col gap-2 ">
-                                    <span>{head?.arabic}</span>
-                                    <span>{head?.english}</span>
-                                </div>
-                                {(head?.arabic === "الموظف" ||
-                                    head?.arabic === "حالة الطلب" ||
-                                    head?.arabic === "العميل" ||
-                                    head?.arabic === "نوع الطلب") && <SearchField />}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {TABLE_ROWS.map(
-                        (
-                            {
-                                last_update_note,
-                                customer_name,
-                                status,
-                                // date_of_reprocessing = "لا يوجد",
-                                date_of_reprocessing = "",
-                                customer_salary_amount,
-                                customer_phone,
-                                order_type,
-                                id,
-                            },
-                            index
-                        ) => {
-                            const isLast = index === TABLE_ROWS.length - 1;
-                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-//   {(loggedUser === 1 || loggedUser === 2) && (
-                            return (
-                                
-                                <tr key={index} onClick={() => navigateToRow(id)}>
-                                   
-                                    <td className={classes}>
-                                    <Button
-                         onClick={()=> handleOpenAddUpdates(id)}
-                        color="green"
-                        className="flex items-center gap-3 float-right"
-                    >
-                        
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                        </svg>
-                        إضافة التحديث
+  const TABLE_HEAD = [
+    {
+      arabic: '',
+    },
+    {
+      arabic: 'الملاحظات',
+    },
+    {
+      arabic: 'الموظف',
+    },
+    {
+      arabic: 'حالة الطلب',
+    },
+    {
+      arabic: 'تاريخ إعادة المعالجة',
+    },
+    {
+      arabic: 'بنك الراتب',
+    },
+    {
+      arabic: 'رقم الحوال',
+    },
+    {
+      arabic: 'العميل',
+    },
+    {
+      arabic: 'نوع الطلب',
+    },
+    {
+      arabic: 'بطاقة تعريف',
+    },
+  ]
+
+  const TABLE_ROWS = allOrders
+
+  const navigate = useNavigate()
+  const navigateToRow = (id) => {
+    navigate(`/dashboard/order_details?order_id=${id}`)
+  }
+
+  return (
+    <Card className='overflow-auto h-full w-full'>
+      <table className='w-full min-w-max table-auto text-left'>
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head, i) => (
+              <th
+                key={i}
+                className='border-b border-blue-gray-100 bg-blue-gray-50 p-3   '>
+                <div className='flex flex-col gap-2 '>
+                  <span>{head?.arabic}</span>
+                  <span>{head?.english}</span>
+                </div>
+                {(head?.arabic === 'الموظف' ||
+                  head?.arabic === 'حالة الطلب' ||
+                  head?.arabic === 'العميل' ||
+                  head?.arabic === 'نوع الطلب') && <SearchField />}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {TABLE_ROWS.map(
+            (
+              {
+                last_update_note,
+                customer_name,
+                status,
+                // date_of_reprocessing = "لا يوجد",
+                date_of_reprocessing = '',
+                customer_salary_amount,
+                customer_phone,
+                order_type,
+                id,
+              },
+              index
+            ) => {
+              const isLast = index === TABLE_ROWS.length - 1
+              const classes = isLast
+                ? 'p-4'
+                : 'p-4 border-b border-blue-gray-50'
+              //   {(loggedUser === 1 || loggedUser === 2) && (
+              return (
+                <tr
+                  key={index}
+                  onClick={(event) => {
+                    if (!event.target.classList.contains('no-click')) {
+                      navigateToRow(id)
+                    }
+                  }}
+                  className='hover:bg-gray-100 cursor-pointer'>
+                  <td className={classes}>
+                    <Button
+                      onClick={() => handleOpenAddUpdates(id)}
+                      color='green'
+                      className='flex items-center gap-3 float-right no-click'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='w-6 h-6'>
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                        />
+                      </svg>
+                      إضافة التحديث
                     </Button>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {last_update_note}
-                                        </Typography>
-                                    </td>
-                                    {(loggedUser === 1 || loggedUser === 2) && (<td className={classes}>
-                                        <Typography
-                                            onClick={() => handleAssign(id)}
-                                            variant="small" color="blue-gray" className="font-normal bg-blue-400 p-2 text-center rounded-lg text-white
-                                            hover:bg-blue-500 cursor-pointer
-                                            ">
-                                            {customer_name}
-                                        </Typography>
-                                    </td>)}
-                                    {(loggedUser === 1 || loggedUser === 2) && (<td className={classes}>
-                                        <Link to={`/dashboard/new_order?order_id=${id}`} key={index}>    <Typography variant="small" color="blue-gray" className="font-normal text-white bg-orange-400 p-2 text-center rounded-lg"
-                                            
-                                        >
-                                            {order_status_translations[status]}
-                                        </Typography></Link>
-                                    </td>)}
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {date_of_reprocessing}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {customer_salary_amount}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {customer_phone}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                   <Typography
-                                             
-                                            variant="small" color="blue-gray" className="font-normal">
-                                            {customer_name}
-                                        </Typography>
-                                       
-                                        
-                                       
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {order_type}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {id}
-                                        </Typography>
-                                    </td>
-                                    
-                                    </tr>
-                                
-                            );
-                        }
-                    )}
-                </tbody>
-            </table>
-        </Card>
-    );
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {last_update_note}
+                    </Typography>
+                  </td>
+                  {(loggedUser === 1 || loggedUser === 2) && (
+                    <td className={classes}>
+                      <Typography
+                        onClick={() => handleAssign(id)}
+                        variant='small'
+                        color='blue-gray'
+                        className='font-normal bg-blue-400 p-2 text-center rounded-lg text-white hover:bg-blue-500 cursor-pointer no-click'>
+                        {customer_name}
+                      </Typography>
+                    </td>
+                  )}
+                  {(loggedUser === 1 || loggedUser === 2) && (
+                    <td className={classes}>
+                      <Link
+                        to={`/dashboard/new_order?order_id=${id}`}
+                        key={index}>
+                        <Typography
+                          variant='small'
+                          color='blue-gray'
+                          className='font-normal text-white bg-orange-400 p-2 text-center rounded-lg no-click'>
+                          {order_status_translations[status]}
+                        </Typography>
+                      </Link>
+                    </td>
+                  )}
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {date_of_reprocessing}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {customer_salary_amount}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {customer_phone}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {customer_name}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {order_type}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant='small'
+                      color='blue-gray'
+                      className='font-normal'>
+                      {id}
+                    </Typography>
+                  </td>
+                </tr>
+              )
+            }
+          )}
+        </tbody>
+      </table>
+    </Card>
+  )
 }
